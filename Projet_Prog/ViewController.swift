c858812bf485beda2c4fe14ce1f2129c7cca309f
BENETEAU <paulbeneteau@hotmail.fr>
2017-10-11 17:27:50 +0200

    Initial Commit

---
+++ Projet_Prog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projet_Prog
+//
+//  Created by Paul Bénéteau on 11/10/2017.
+//  Copyright © 2017 Paul Bénéteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
