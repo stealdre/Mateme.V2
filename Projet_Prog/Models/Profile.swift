3082a59e2364ed636547396af21b28d2b2a75e51
Maxence <maxence@MacBook-Pro-de-Maxence.local>
2017-11-17 13:47:52 +0100

    bdd profile

---
+++ Projet_Prog/Models/Profile.swift
@@ -0,0 +1,58 @@
+//
+//  Profile.swift
+//  Projet_Prog
+//
+//  Created by Maxence on 14/11/2017.
+//  Copyright © 2017 Hemispher. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+
+class Profile {
+	
+	var name: String = ""
+	var profilPicPath: String = ""
+	
+	struct History {
+		
+		var gameDate: String = ""
+		var gameID: String = ""
+		var gameName: String = ""
+		var mateID: String = ""
+		var mateName: String = ""
+		var rate: Float = 0.0
+	}
+	
+	var historiesArray = [History]()
+
+	var ref: DatabaseReference?
+	
+	init (snapshot: DataSnapshot) {
+		
+		ref = snapshot.ref
+		
+		let data = snapshot.value as! [String: Any]
+		
+		name = data["name"]! as! String
+		profilPicPath = data["profilPicPath"]! as! String
+		
+		let histories = data["history"] as! [String : [String: Any]]
+		
+		for history in histories {
+			var item = History()
+			let array = history.value
+			
+			item.gameDate = history.key
+			item.gameID = array["gameID"] as! String
+			item.mateID = array["mateID"] as! String
+			item.rate = array["rate"] as! Float
+			
+			historiesArray.append(item)
+		}
+		//print(histories)
+	}
+	
+	
+}
